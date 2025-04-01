fdc496a33af60ff22eaa97a0302ef6b92d9713ad
manhht-PH56912 <manh.dev1517@gmail.com>
2025-04-01 12:49:52 +0700

    update

--- Assets/00 Game/_Scripts/Configs/FollowerPlayer.cs
+++ Assets/00 Game/_Scripts/Configs/FollowerPlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Game.Configs;
 
 public class FollowerPlayer : MonoBehaviour
 {
---
+++ Assets/00 Game/_Scripts/Configs/GameConfigs.cs
@@ -0,0 +1,27 @@
+namespace Game.Configs
+{
+    public class GameConfigs
+    {
+        public const string PlayerPrefsKey = "PlayerPrefsKey"; // Key for PlayerPrefs
+        public const string GameVersion = "1.0.0";
+        public const string GameName = "MyGame";
+        public const string GameAuthor = "YourName";
+        public const string GameDescription = "This is a sample game description.";
+    }
+
+    public class LayerNames
+    {
+        public const string Player = "Player";
+        public const string Enemy = "Enemy";
+        public const string Ground = "Ground";
+        public const string Effect = "Effect";
+        public const string UI = "UI";
+    }
+
+    public class Tags
+    {
+        public const string Player = "Player"; 
+        public const string Enemy = "Enemy";
+        public const string Item = "Item";
+    }
+}
